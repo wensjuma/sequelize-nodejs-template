b586789f4261341675654ff253dd152f65c7b4d5
Wenslaus <wabukala.wenslaus@ekenya.co.ke>
2022-03-14 10:45:26 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+const db = require("./src/models");
+db.sequelize.sync();
+
+/**If existing tables need to dropped and the database resynced */
+// db.sequelize.sync({
+//     force: true
+// }).then(() => {
+//     console.log("Drop and re-sync db.");
+// });
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to WensJuma application."
+    });
+});
+
+require("./src/routes/routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
